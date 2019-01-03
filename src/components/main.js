44a148ed28017b1216bfbfad88cf25427615bd35
Robby Lewis <robbyklew@gmail.com>
2019-01-03 10:20:44 -0800

    updated fetch requests

--- src/components/main.js
+++ src/components/main.js
@@ -63,7 +63,7 @@ export default class Main extends React.Component {
             teams: [...this.state.teams, { team }]
         });
         console.log('Teams', team)
-        fetch('http://localhost:8080/teams/' + localStorage.getItem('username'), {
+        fetch(`${API_BASE_URL}/teams`/ + localStorage.getItem('username'), {
             method: "PUT",
             body: JSON.stringify({
                 team: team,
